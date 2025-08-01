25d60b77ecf8e5d155323dce9d5dd11451755964
hannahliu123 <hannah.liu2010@gmail.com>
2025-08-01 00:56:47 -0400

    YAYA SEO SETUP DONE??
    def not dont but uh

---
+++ generatePosts.js
@@ -0,0 +1,52 @@
+import admin from "firebase-admin";     // needed because this runs in Node.js, not browser
+import serviceAccount from "./serviceAccountKey.json" with { type: "json" };    // must explicitly use
+import fs from "fs";        // Node.js filesystem module to add .html file
+import puppeteer from "puppeteer";
+
+const credentials = {
+    credential: admin.credential.cert(serviceAccount)
+};
+
+admin.initializeApp(credentials);
+const db = admin.firestore();
+const postsFolder = "./public/posts/";
+const postIDs = [];
+
+async function getPostIDs() {
+    const snapshot = await db.collection("posts").get();
+    snapshot.forEach(doc => {
+        postIDs.push(doc.data().id);
+    });
+}
+
+async function createPosts() {
+    await getPostIDs();
+    console.log("Found post IDs:", postIDs);
+
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    // Make sure posts folder exists
+    if (!fs.existsSync(postsFolder)) {
+        fs.mkdirSync(postsFolder);  // make directory synchronously
+    }
+
+    for (const id of postIDs) {   // because async
+        const url = `https://blog.byhannahliu.com/post?id=${id}`;
+        console.log(`Generating snapshot for post: ${id}...`);
+
+        await page.goto(url, {
+            waitUntil: "networkidle0",
+            timeout: 120000     // 2 mins
+        });    // Wait for network to be idle (fully loaded)
+        const html = await page.content();
+
+        // Write to local file
+        fs.writeFileSync(`${postsFolder}${id}.html`, html);
+        console.log(`Saved ${postsFolder}${id}.html`);
+    }
+
+    await browser.close();
+}
+
+createPosts();
