ca494d6fe4bd0b8a965e4f563a5a047affb2b99e
hannahliu123 <hannah.liu2010@gmail.com>
2025-09-25 21:01:05 -0400

    notifications ui!!!
    only for admin (mee!)

--- public/admin/notifications.js
+++ public/admin/notifications.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
-import { getAuth, signInWithEmailAndPassword, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-auth.js";
+import { getFirestore, getDocs, collection } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-firestore.js";
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-auth.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyCs0tT5ACiOgNBkpmbgpt_9d92lfGexy0M",
@@ -12,12 +13,67 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 const auth = getAuth(app);
 
-onAuthStateChanged(auth, (user) => {
+onAuthStateChanged(auth, async (user) => {
     if (user) {
         console.log("You are logged in!")
         console.log("UID:", user.uid);
+        document.body.innerHTML = `
+            <h1 id="header">Notifications</h1>
+            <div id="notif-container"></div>
+        `;
+
+        const notifContainer = document.getElementById("notif-container");
+        const logsRef = collection(db, "logs");
+        const snapshot = await getDocs(logsRef);
+        let allNotifs = []; // array of all the notifications
+
+        snapshot.forEach(doc => {
+            allNotifs.push(doc.data());
+        });
+
+        // sort all notifs from oldest -> most recent
+        allNotifs.sort((a, b) => {
+            const timeA = a.timestamp;
+            const timeB = b.timestamp;
+
+            if (timeA < timeB) return -1;   // neg value means a should come BEFORE b
+            if (timeA > timeB) return 1;    // pos value means a should come AFTER b
+            return 0;       // 0 or NaN means a and b are considered EQUAL
+        });
+
+        var add_cnt = 1;
+        allNotifs.forEach(notif => {
+            const container = document.createElement("div");
+            container.classList.add("notif");
+            container.innerHTML = `
+                <h2 class="count"></h2>
+                <p><span class="bold">User: </span>
+                    <span class="user"></span></p>
+                <p><span class="bold">Date: </span>
+                    <span class="date"></span></p>
+                <p><span class="bold">Post: </span>
+                    <a class="post"></a></p>
+                <p><span class="bold">Content: </span>
+                    <span class="content"></span></p>
+            `;
+
+            if (notif.type === "add comment") {
+                container.querySelector(".count").textContent = "Add Comment #" + add_cnt;
+                add_cnt++;
+            } else container.querySelector(".count").textContent = "Remove Comment";
+            container.querySelector(".user").textContent = notif.user;
+            container.querySelector(".date").textContent = notif.timestamp.toDate();
+            container.querySelector(".post").textContent = notif.postID;
+            container.querySelector(".post").setAttribute("href", "https://blog.byhannahliu.com/posts/" + notif.postID);
+            container.querySelector(".content").textContent = notif.content;
+
+            notifContainer.prepend(container);
+        });
+
+        console.log(allNotifs);
     } else {
         console.log("You are NOT logged in.")
     }
